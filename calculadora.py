f7b8a139b127c473125d1ab84b45b307906b3405
olordecoelho <olordecoelho@gmail.com>
2023-03-30 15:57:18 -0400

    usei loops de while para sets e escrevi a formula

--- calculadora.py
+++ calculadora.py
@@ -4,9 +4,27 @@ tempo = 0
 
 
 
-while capitalinicial <=0:
+while True:
     capitalinicial = float(input("Insira o capital inicial: "))
-    if capitalinicial <=0:
-        print("Capital inicial deve ser maior do que 0")
+    if capitalinicial <0:
+        print("Capital inicial não pode ser menor do que 0")
+    else:
+        break
+
+while True:
+    juros = float(input("Insira a taxa de juros: "))
+    if juros <0:
+        print("a taxa de juros não pode ser menor do que 0")
+    else:
+        break
         
-prit(capitalinicial)
+while True:
+    tempo = int(input("Insira por quanto tempo seu capital ficará aplicado: "))
+    if tempo <0:
+        print("tempo não pode ser menor do que 0")
+    else:
+        break
+
+
+total = capitalinicial * pow((1 + juros / 100), tempo)
+print(F"Seu saldo depois de {tempo} anos será R${total:.2f}")
